db8add25cc16795b6ab163601968a99019bcde3d
SoulWalker5 <shrituon@gmail.com>
2019-12-13 02:14:27 +0200

    init

---
+++ Lesson_2Module_2/GetPart.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+     class GetPart : Menu
+    {
+        protected Car car;
+        public GetPart(Car car)
+        {
+            this.car = car;
+        }
+        protected override void InternalLogic()
+        {
+            car.GetPart();
+        }
+    }
+}
---
+++ Lesson_2Module_2/Part.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Part
+    {
+        public Part()
+        {
+        }
+
+        public int Wheel { get; set; }
+        public int SteeringWheel { get; set; }
+        public int Engine { get; set; }
+        public int Seat { get; set; }
+
+    }
+}
